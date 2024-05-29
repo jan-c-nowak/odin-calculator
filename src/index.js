24eb5ff2263683bafa63279f450fbad0baab735c
jan-c-nowak <84571576+jan-c-nowak@users.noreply.github.com>
2024-05-29 13:43:42 +0200

    add operations

---
+++ src/index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+}
+
+let operandA;
+let operandB;
+let operation;
+
+function operate(operation, operandA, operandB) {
+    switch (operation) {
+        case "add":
+            return add(operandA, operandB);
+        case "subtract":
+            return subtract(operandA, operandB);
+        case "multiply":
+            return multiply(operandA, operandB);
+        case "divide":
+            return divide(operandA, operandB);
+        default:
+            throw new Error("Invalid operation");
+    }
+}
+
